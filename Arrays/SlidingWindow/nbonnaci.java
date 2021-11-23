44761d9b94f7d1aa2f43361189dbd4d2906a5064
Mehul Singhal <mehul.singhal16@gmail.com>
2021-11-23 17:19:41 +0530

    N bonnaci series code added (the general form of fibonnaci series)

--- Arrays/SlidingWindow/nbonnaci.java
+++ Arrays/SlidingWindow/nbonnaci.java
@@ -2,12 +2,24 @@ package Arrays.SlidingWindow;
 
 public class nbonnaci {
 
-    public static void bonacciSeries(int arr[], int m){
-        
+    public static void bonacciSeries(int m, int n){
+        int arr[] = new int[m];
+        for(int i = 0;i<m;i++){
+            arr[i] = 0;
+        }
+        arr[n] = 1;
+        arr[n-1] = 1;
+        for(int i = n+1;i<m;i++){
+            arr[i] = 2*arr[i-1] - arr[i-n-1];
+        }
+        for(int i = 0;i<m;i++){
+            System.out.println(arr[i]+ " ");
+        }
     }
 
     public static void main(String args[]){
-
+        int N = 5, M = 15;
+        bonacciSeries(M, N);
     }
     
 }
