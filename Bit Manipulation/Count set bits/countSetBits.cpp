4242c1fc2b79889e8605300c5fc7ff80aeccc6b8
Mehul Singhal <mehul.singhal16@gmail.com>
2022-02-12 17:42:51 +0530

    Count set bits implemented in c++

--- Bit Manipulation/Count set bits/countSetBits.cpp
+++ Bit Manipulation/Count set bits/countSetBits.cpp
@@ -18,15 +18,32 @@ int countSetBitBasic(int n){
 int countSetBitsOptimised(int n){
     int result = 0; 
     while(n>0){
-        n = (n&(n-1));
+        n = (n & (n-1));
         result++;
     }
     return result;
+}
+
+
+//The look up table method
+int table[256];
+
+void initialize(){
+    table[0] = 0; 
+    for(int i = 0;i<256;i++){
+        table[i] = (i&1) + table[i/2];
+    }
+}
+
+int countSetBitsUsingLookUpTable(int n){
+    initialize();
+    return table[n & 0xff] + table[(n>>8) & 0xff] +table[(n>>16) & 0xff] + table[n>>24];
 }
 
 int main()
 {
     cout<<countSetBitBasic(5)<<endl;
     cout<<countSetBitsOptimised(7)<<endl;
+    cout<<countSetBitsUsingLookUpTable(9)<<endl;
     return 0;
 }
