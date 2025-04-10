3aea8c0831f7b141fbc1482ccc0952ffd6f4b5bf
mehul-singhal <mehulbtp@gmail.com>
2025-04-10 15:07:13 +0530

    Comments updated with explanation for moore's

--- Arrays/MajorityElement/majorElement.java
+++ Arrays/MajorityElement/majorElement.java
@@ -4,6 +4,11 @@ public class majorElement {
     //This is moore's voting algorithm 
     //In this algorithm the votes keep cancelling each other to find the majority element. 
     //This comes in handy where you need to find the majority element.
+    //The basic idea here is that you assume that one of the element is the majority and increase it if you find the same number
+    //And decrease it if you find different number
+    //If the assumed majority becomes 0 that means it wasn't the majority for the elements till now
+    //Keep doing this and we find a element which is going to be the majority -> This is phase one of the algorithm
+    //Post this we check the number of times the element is occuring and if it is more that n/2
     public static Integer majorityElement(Integer arr[]){
         Integer res = 0; 
         Integer count = 1;
