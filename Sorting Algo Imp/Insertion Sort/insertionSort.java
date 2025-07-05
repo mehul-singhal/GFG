e89a0fe111717464fc984dc53144c38b64096afa
mehul-singhal <mehulbtp@gmail.com>
2025-07-06 02:10:57 +0530

    Inssertion sort

---
+++ Sorting Algo Imp/Insertion Sort/insertionSort.java
@@ -0,0 +1,34 @@
+public class insertionSort {
+    public static void insertionSortForArr(int[] arr) {
+        int n = arr.length;
+        for(int i = 0; i<n; i++){
+            int j = i;
+            while(j>0 && arr[j-1] > arr[j]){
+                int temp = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+    }
+
+     public static void main(String[] args) {
+    // Example array to be sorted
+    int[] arr = {64, 25, 12, 22, 11};
+    // Print original array
+    System.out.println("Original array:");
+    printArray(arr);
+    // Perform selection sort
+    insertionSortForArr(arr);
+    // Print sorted array
+    System.out.println("Sorted array:");
+    printArray(arr);
+}
+// Helper function to print an array
+public static void printArray(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+        System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+}
+}
