38e1981d27cace5904d34a8fb457d1732c34b480
mehul-singhal <mehulbtp@gmail.com>
2025-07-15 17:16:13 +0530

    Bubble Sort Question GFG Done

--- Sorting Algo Imp/Bubble Sort/bubbleSort.java
+++ Sorting Algo Imp/Bubble Sort/bubbleSort.java
@@ -15,6 +15,21 @@ public class bubbleSort {
                 break;
             }
         }
+
+        // for(int i = 0; i<n-1; i++){
+        //     int didSwap = 0;
+        //     for(int j = 0; j<n-i-1; j++){
+        //         if(arr[j] > arr[j+1]){
+        //             int temp = arr[j];
+        //             arr[j] = arr[j+1];
+        //             arr[j+1] = temp;
+        //             didSwap = 1;
+        //         }
+        //     }
+        //     if(didSwap == 0){
+        //         break;
+        //     }
+        // }
     }
 
     public static void bubbleSortRecursive(int[] arr, int n) {
